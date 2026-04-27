9117055a751e850e9204ea560398fda686df2287
Eduardo Reynoso <eduardo_3reynoso@hotmail.com>
2026-04-26 21:42:11 -0700

    set up create table scripts

--- scripts/init.sql
+++ scripts/init.sql
@@ -10,6 +10,16 @@ CREATE TABLE IF NOT EXISTS user (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS business_type (
+    business_type_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    description TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS businesses (
     business_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
@@ -21,5 +31,168 @@ CREATE TABLE IF NOT EXISTS businesses (
     CONSTRAINT fk_business_user
         FOREIGN KEY (user_id)
         REFERENCES users(id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    
+    CONSTRAINT fk_business_type
+        FOREIGN KEY (business_type_id)
+        REFERENCES business_types(id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS product_categories (
+    product_category_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    business_id UUID NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_category_business
+        FOREIGN KEY (business_id)
+        REFERENCES businesses(business_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT uq_category_name_per_business
+        UNIQUE (business_id, name)
+);
+
+CREATE TABLE IF NOT EXISTS units (
+    unit_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code VARCHAR(30) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    symbol VARCHAR(10),
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    business_id UUID NOT NULL,
+    sku VARCHAR(50) NOT NULL,
+    name VARHCAR(100) NOT NULL,
+    description TEXT,
+    product_category_id UUID NULL,
+    unit_id UUID NULL,
+    cost_price NUMERIC(12, 2) NOT NULL DEFAULT 0,
+    sale_price NUMERIC(12, 2) NOT NULL DEFAULT 0,
+    current_stock INTEGER NOT NULL DEFAULT 0,
+    minimum_stock INTEGER NOT NULL DEFAULT 0,
+    maximum_stock INTEGER,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_product_business
+        FOREIGN KEY (unit_id)
+        REFERENCES businesses(business_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_product_category
+        FOREIGN KEY (product_category_id)
+        REFERENCES product_categories(product_category_id)
+        ON DELETE SET NULL,
+
+    CONSTRAINT fk_product_unit
+        FOREIGN KEY (unit_id)
+        REFERENCES units(unit_id)
+        ON DELETE SET NULL,
+
+    CONSTRAINT uq_product_business_sku
+        UNIQUE (business_id, sku)
+);
+
+CREATE TABLE IF NOT EXISTS order_statuses (
+    order_status_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    business_id UUID NOT NULL,
+    order_status_id UUID NOT NULL,
+    order_folio VARCHAR(100) NOT NULL,
+    customer_name VARCHAR(150) NOT NULL,
+    customer_email VARCHAR(255),
+    customer_phone VARCHAR(30),
+    order_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_order_business
+        FOREIGN KEY (business_id)
+        REFERENCES businesses(business_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_order_status
+        FOREIGN KEY (order_status_id)
+        REFERENCES order_statuses(order_status_id)
+        ON DELETE RESTRICT,
+
+    CONSTRAINT uq_order_business_folio
+        UNIQUE (business_id, order_folio)
+);
+
+CREATE TABLE order_items (
+    order_items_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id UUID NOT NULL,
+    product_id UUID NOT NULL,
+    quantity INTEGER NOT NULL,
+    unit_price NUMERIC(12, 2) NOT NULL,
+    total NUMERIC(12, 2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_order_item_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_order_item_product
+        FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+        ON DELETE RESTRICT,
+
+    CONSTRAINT chk_order_item_quantity
+        CHECK (quantity > 0),
+
+    CONSTRAINT chk_order_item_total
+        CHECK (total >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS movement_types (
+    movement_type_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS inventory_movements (
+    inventory_movement_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_id UUID NOT NULL,
+    movement_type_id UUID NOT NULL,
+    quantity INTEGER NOT NULL,
+    reason VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_inventory_movement_product
+        FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_inventory_movement_type
+        FOREIGN KEY (movement_type_id)
+        REFERENCES movement_types(movement_type_id)
+        ON DELETE RESTRICT,
+
+    CONSTRAINT chk_inventory_movement_quantity
+        CHECK (quantity > 0)
 );
